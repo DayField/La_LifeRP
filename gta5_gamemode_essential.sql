d7e7e783ea416acef9e9aa242912e874a06159b1
DayField <dayfield14@gmail.com>
2017-07-08 03:01:44 +0200

    Add files via upload

--- gta5_gamemode_essential.sql
+++ gta5_gamemode_essential.sql
@@ -104,6 +104,46 @@ INSERT INTO `jobs` VALUES ('10', 'Policier', '0');
 INSERT INTO `jobs` VALUES ('11', 'Fossoyeur', '0');
 INSERT INTO `jobs` VALUES ('12', 'Préposé à la morgue', '0');
 INSERT INTO `jobs` VALUES ('13', 'Ambulancier', '500');
+INSERT INTO `jobs` VALUES ('14', 'Depanneur', '0');
+
+-- ----------------------------
+-- Table structure for taxi
+-- ----------------------------
+
+CREATE TABLE `coffretaxi` (
+  `id` int(11) NOT NULL,
+  `identifier` varchar(50) NOT NULL,
+  `solde` varchar(10) NOT NULL,
+  `dirtysolde` varchar(10) NOT NULL,
+  `lasttransfert` varchar(10) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+INSERT INTO `coffretaxi` (`id`, `identifier`, `solde`, `dirtysolde`, `lasttransfert`) VALUES
+(1, '0', '500', '400', '10');
+
+ALTER TABLE `coffretaxi`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+-- ----------------------------
+-- Table structure for Depanneur
+-- ----------------------------
+
+CREATE TABLE `coffredep` (
+  `id` int(11) NOT NULL,
+  `identifier` varchar(50) NOT NULL,
+  `solde` varchar(10) NOT NULL,
+  `dirtysolde` varchar(10) NOT NULL,
+  `lasttransfert` varchar(10) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+INSERT INTO `coffredep` (`id`, `identifier`, `solde`, `dirtysolde`, `lasttransfert`) VALUES
+(1, '0', '500', '400', '10');
+
+ALTER TABLE `coffredep`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
 
 -- ----------------------------
 -- Table structure for outfits
